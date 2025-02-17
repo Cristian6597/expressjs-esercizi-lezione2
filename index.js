b5e518657a588f25591712154eca2202b255351f
Cristian6597 <cristianercolani6597@gmail.com>
2025-02-17 17:08:03 +0100

    Esercizio finito

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ import fs from 'fs';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+app.get('/frase', (req, res) => {
+    try {
+        //Prendo il dbjson lo legge e lo parsa per poterlo utilizzare
+        let dbText = fs.readFileSync('./db.json');
+        let dbJson = JSON.parse(dbText);
+
+        // Sceglie una frase casuale dal db
+        const randomIndex = Math.floor(Math.random() * dbJson.frase.length); //prende casualmente un elemento dal db con Math.random, Math.floor server per arrotondare il numero scelto
+        const randomFrase = dbJson.frase[randomIndex];
+
+        return res.json(randomFrase); //mi restituisce la frase
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Errore server' }); //gestisce lo status 500 del server
+    }
+});
 
 app.listen(PORT, () => {
-    console.log("listening port: " + PORT);
-})
+    console.log("listening on port: " + PORT);
+});
